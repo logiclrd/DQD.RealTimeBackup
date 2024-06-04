234a4f68b80be8f2164daa63eaeb3f0590044a14
Jonathan Gilbert <logic@deltaq.org>
2024-06-04 14:59:59 -0500

    Reformatted App.axaml.cs in DeltaQ.RTB.UserInterface to use a namespace block instead of a namespace directive.

--- DeltaQ.RTB.UserInterface/App.axaml.cs
+++ DeltaQ.RTB.UserInterface/App.axaml.cs
@@ -7,61 +7,63 @@ using Avalonia.Markup.Xaml;
 
 using ReactiveUI;
 
-namespace DeltaQ.RTB.UserInterface;
-
-public partial class App : Application
+namespace DeltaQ.RTB.UserInterface
 {
-	public override void Initialize()
+	public partial class App : Application
 	{
-		AvaloniaXamlLoader.Load(this);
-	}
+		public override void Initialize()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
 
-	public App()
-	{
-		ShowWindowCommand = ReactiveCommand.Create(ShowWindow);
-		PauseMonitoringCommand = ReactiveCommand.Create(PauseMonitoring);
-		ResumeMonitoring_ProcessBufferedEvents_Command = ReactiveCommand.Create(() => ResumeMonitoring(true));
-		ResumeMonitoring_DiscardBufferedEvents_Command = ReactiveCommand.Create(() => ResumeMonitoring(false));
-		ExitCommand = ReactiveCommand.Create(Exit);
+		public App()
+		{
+			ShowWindowCommand = ReactiveCommand.Create(ShowWindow);
+			PauseMonitoringCommand = ReactiveCommand.Create(PauseMonitoring);
+			ResumeMonitoring_ProcessBufferedEvents_Command = ReactiveCommand.Create(() => ResumeMonitoring(true));
+			ResumeMonitoring_DiscardBufferedEvents_Command = ReactiveCommand.Create(() => ResumeMonitoring(false));
+			ExitCommand = ReactiveCommand.Create(Exit);
 
-		DataContext = this;
-	}
+			DataContext = this;
+		}
 
-	public ICommand ShowWindowCommand { get; }
-	public ICommand PauseMonitoringCommand { get; }
-	public ICommand ResumeMonitoring_ProcessBufferedEvents_Command { get; }
-	public ICommand ResumeMonitoring_DiscardBufferedEvents_Command { get; }
-	public ICommand ExitCommand { get; }
+		public ICommand ShowWindowCommand { get; }
+		public ICommand PauseMonitoringCommand { get; }
+		public ICommand ResumeMonitoring_ProcessBufferedEvents_Command { get; }
+		public ICommand ResumeMonitoring_DiscardBufferedEvents_Command { get; }
+		public ICommand ExitCommand { get; }
 
-	public MainWindow? MainWindow { get; set; }
+		public MainWindow? MainWindow { get; set; }
 
-	void ShowWindow()
-	{
-		MainWindow ??= new MainWindow();
-		MainWindow.Show();
-	}
+		void ShowWindow()
+		{
+			MainWindow ??= new MainWindow();
+			MainWindow.Show();
+		}
 
-	void PauseMonitoring()
-	{
-	}
+		void PauseMonitoring()
+		{
+		}
 
-	void ResumeMonitoring(bool processBufferedEvents)
-	{
-	}
+		void ResumeMonitoring(bool processBufferedEvents)
+		{
+		}
 
-	void Exit()
-	{
-		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-			desktop.Shutdown();
-	}
+		void Exit()
+		{
+			if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+				desktop.Shutdown();
+		}
 
-	public override void OnFrameworkInitializationCompleted()
-	{
-		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-			desktop.ShutdownMode = Avalonia.Controls.ShutdownMode.OnExplicitShutdown;
+		public override void OnFrameworkInitializationCompleted()
+		{
+			if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+				desktop.ShutdownMode = Avalonia.Controls.ShutdownMode.OnExplicitShutdown;
 
-		base.OnFrameworkInitializationCompleted();
+			base.OnFrameworkInitializationCompleted();
 
-		ShowWindow();
+			ShowWindow();
+		}
 	}
 }
+
