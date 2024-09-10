7086ed7c98896938773d73d834f03722a3b93b2b
Jonathan Gilbert <logic@deltaq.org>
2024-09-10 16:23:21 -0500

    Revert "Reworked DQD.RealTimeBackup.Web to use LettuceEncrypt."
    This reverts commit ec89e830885efb8c3e72917919b56680dfcf3a06.

--- DQD.RealTimeBackup.Web/Program.cs
+++ DQD.RealTimeBackup.Web/Program.cs
@@ -51,12 +51,6 @@ namespace DQD.RealTimeBackup.Web
 						if (!string.IsNullOrWhiteSpace(parameters.WebAccessCertificatePath)
 						 && Directory.Exists(parameters.WebAccessCertificatePath))
 						{
-							webHostBuilder.ConfigureServices(
-								services =>
-								{
-									services.AddLettuceEncrypt();
-								});
-
 							webHostBuilder.ConfigureKestrel(
 								serverOptions =>
 								{
@@ -64,7 +58,7 @@ namespace DQD.RealTimeBackup.Web
 										httpsOptions =>
 										{
 											httpsOptions.AllowAnyClientCertificate();
-											httpsOptions.UseLettuceEncrypt(serverOptions.ApplicationServices);
+											httpsOptions.ServerCertificate = LoadServerCertificate(parameters.WebAccessCertificatePath);
 										});
 								});
 						}
@@ -149,6 +143,14 @@ namespace DQD.RealTimeBackup.Web
 			}
 
 			host.Run();
+		}
+
+		static X509Certificate2 LoadServerCertificate(string path)
+		{
+			string certificatePem = File.ReadAllText(Path.Combine(path, "cert.pem"));
+			string privateKeyPem = File.ReadAllText(Path.Combine(path, "privkey.pem"));
+
+			return X509Certificate2.CreateFromPem(certificatePem, privateKeyPem);
 		}
 
 		class AuthenticateWithBackblazeFilter : IStartupFilter
